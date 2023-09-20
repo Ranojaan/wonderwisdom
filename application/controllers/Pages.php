a8fedfc09f41d437ae1f9bb9b4f098453384bd2b
rano <ranojaan2602@gmail.com>
2023-09-20 21:56:07 +0530

    2nd commit

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -4,6 +4,10 @@
         if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
             show_404();
         }
-        
+        $data ['title'] =ucfirst($page);
+
+        $this->load->view('templates/header');
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer');
     }
    }
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,19 @@
 <html>
     <head>
         <title>Wonder Wisdom</title>
+        <link rel="stylesheet" href="https://bootswatch.com/5/flatly/bootstrap.min.css">
     </head>
 <body>
+    <nav class="navbar navbar-inverse"> 
+        <div class="container">
+            <div class="navbar header">
+                <a class="navbar-brand" href="/">WonderWisdom</a>
+            </div>
+            <div id="navbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="/">Home</li>
+                    <li><a href="/about">About</li>
+                </ul>
+            </div>
+        </div>
+    </nav>
